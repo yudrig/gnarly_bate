d01d1390895418343192cb954c6271d2e8df2f8e
yudrig <uyled@cock.li>
2018-03-07 20:50:51 -0500

    Now works both when called from command line and when used in a class

--- sendMail.py
+++ sendMail.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python
 
+#TODO:make this work both alone and with other class
 import getpass
 import smtplib
 import sys
 
-
 #This sends SMTP mail via command line arguments.
 #args: senderName, senderAddress, recieverName, receiverAddress, subject, message
 
-message = """From: %s <%s>
+def sendIt(senderName, senderAddress, recieverName, receiverAddress, subject, message):
+    message = """From: %s <%s>
 To: %s <%s>
 Subject: %s
 
-%s""" % (sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
-(pixel_byte_string, mime_type) = pytracking.get_open_tracking_pixel()
-print message
+%s""" % (senderName, senderAddress, recieverName, receiverAddress, subject, message)
+    #(pixel_byte_string, mime_type) = pytracking.get_open_tracking_pixel()
+    #print message
 
-try:
-	#TODO: Send mail from arbitrary server
-	smtpObj = smtplib.SMTP('localhost')
-	smtpObj.sendmail(getpass.getuser()+'@adelphi.edu',sys.argv[4],message)
-	sys.exit(0)
-except smtplib.SMTPException:
-	sys.exit(1)
-#import pytracking
-#(pixel_byte_string, mime_type) = pytracking.get_open_tracking_pixel()
+    try:
+        #TODO: Send mail from arbitrary server
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.sendmail(getpass.getuser()+'@compsci.adelphi.edu',receiverAddress,message)
+        sys.exit(0)
+    except smtplib.SMTPException:
+        print("Mail not sent!")
+        sys.exit(1)
+    #import pytracking
+    #(pixel_byte_string, mime_type) = pytracking.get_open_tracking_pixel()
+    return 1
+
+def main():
+    sendIt(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
+
+if __name__ == '__main__': main()
