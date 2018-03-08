e300b1a03235581867f5806f3e821583a43427d0
yudrig <uyled@cock.li>
2018-03-08 18:01:44 -0500

    sendMail.py now has two methods. sendIt() sends from one address to another. sendMany() takes a list of recipients and recipient addresses as the first two arguments and calls sendIt() for each.

--- sendMail.py
+++ sendMail.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python
 
-#TODO:make this work both alone and with other class
-import getpass
-import smtplib
-import sys
-
 #This sends SMTP mail via command line arguments.
-#args: senderName, senderAddress, recieverName, receiverAddress, subject, message
+#args: receiverName, receiverAddress, senderName, senderAddress, subject, message
 
-def sendIt(senderName, senderAddress, recieverName, receiverAddress, subject, message):
+def sendIt(receiverName, receiverAddress, senderName, senderAddress, subject, message):
+    import getpass
+    import smtplib
+    import sys
     message = """From: %s <%s>
 To: %s <%s>
 Subject: %s
 
-%s""" % (senderName, senderAddress, recieverName, receiverAddress, subject, message)
+%s""" % (receiverName, receiverAddress, senderName, senderAddress, subject, message)
     #(pixel_byte_string, mime_type) = pytracking.get_open_tracking_pixel()
     #print message
 
@@ -21,13 +19,19 @@ Subject: %s
         #TODO: Send mail from arbitrary server
         smtpObj = smtplib.SMTP('localhost')
         smtpObj.sendmail(getpass.getuser()+'@compsci.adelphi.edu',receiverAddress,message)
-        sys.exit(0)
+        return 1
     except smtplib.SMTPException:
-        print("Mail not sent!")
-        sys.exit(1)
+        return -1
     #import pytracking
     #(pixel_byte_string, mime_type) = pytracking.get_open_tracking_pixel()
-    return 1
+
+def sendMany(recipients, recipientAddresses, senderName, senderAddress, subject, message):
+    import getpass
+    import smtplib
+    import sys
+    i = 0
+    for receiver in recipients:
+        sendIt(receiver, recipientAddresses[i], senderName, senderAddresssubject, message)
 
 def main():
     sendIt(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
