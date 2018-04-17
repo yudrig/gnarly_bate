c45d9a3c5f60338769efac607602feacd9bbe78f
thomasthetrain94 <36656088+thomasthetrain94@users.noreply.github.com>
2018-04-17 17:36:18 -0400

    Create wrapper.py

---
+++ wrapper.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2
+import searchUsers
+import searchTemplates
+import imageCreation
+#searchUsers()
+#searchTemplates()
+imageCreation.imageCreation((searchTemplates.searchTemplates(1,101),searchUsers.searchUsers(1,1,3)))
+
