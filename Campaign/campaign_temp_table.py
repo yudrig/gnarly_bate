00e6f233a4a8b3f053c64e0756f515bc429af418
yudrig <uyled@cock.li>
2018-05-01 16:06:17 -0400

    Organized components of project, added __init__.py's

--- Campaign/campaign_temp_table.py
+++ Campaign/campaign_temp_table.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 
-#This creates the campaign tables based on parameters, which will be used to track emails receptions
+#creates the campaign tables based on parameters, which will be used to track emails receptions
 #Takes the name of the campaign, the campaign end date
 import sys
 import psycopg2
@@ -50,14 +50,18 @@ CREATE TABLE campaign_%s (
     return 1
 
 def table_updating(campaignID,userIDs,incident):
-    cur = psql_connect()
-    
-    command = """BEGIN TRANSACTION;
-    UPDATE campaign_%s SET %s = TRUE, date_%s = current_timestamp WHERE userID = ;
-    COMMIT;""" % (campaignID,incident)
-    command = command  + '%s'
-    for user in userIDs:
-        cur.execute(command,(user,))
+    try:
+        conn = psycopg2.connect("dbname = 'groupg' user = '%s'" % getpass.getuser())
+    except:
+        print "Unable to connect to DB!"
+        return -1
+    cur = conn.cursor() 
+    cur.execute("select * from information_schema.tables where table_name=%s", ("campaign_"+campaignID,))
+    if cur.rowcount:    
+       cur.execute("""BEGIN TRANSACTION;
+       UPDATE %s SET %s = TRUE, date_%s = current_timestamp WHERE userID = %s;
+       COMMIT;""" % ( "campaign_"+campaignID , incident , incident , user))
+
 
 def end_campaign(campaignID):
     cur = psql_connect()
