343095b0b33b3786738d1024f76f380bed6798d3
yudrig <uyled@cock.li>
2018-05-09 20:24:56 -0400

    Fixed runCamp reference to table_updating

--- CronParse/runCamp.py
+++ CronParse/runCamp.py
@@ -11,10 +11,10 @@ if pix is not None:
 	for p in pix:
 		mail = p[0]
 		user = p[1]
-		table_updating.table_updating(mail,user,"opened")
+		campaign_temp_table.table_updating(mail,user,"opened")
 if url is not None:
 	for u in url:
 		mail = u[0]
 		user = u[1]
-		table_updating.table_updating(mail,user,"failed")
+		campaign_temp_table.table_updating(mail,user,"failed")
 
